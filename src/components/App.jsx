e294c0f58d46c39ceb21c1c2456e61bf464dfe9b
Denys-Burduzha <106137997+Denys-Burduzha@users.noreply.github.com>
2023-01-18 15:18:31 +0100

    5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,30 @@
-// import {  Route , Routes } from "react-router-dom";
-// import { Layout } from "./Layout/Layout";
-// import { Home } from "./Home/Home";
-// import { GiphsSearch } from "./GiphsSearch/GiphsSearch"
-// import React from "react";
-// import {GiphDetails} from "./GiphsDetails/GiphsDetails"
-// import {GiphAuthor} from "./GiphAuthor/GiphAuthor.jsx"
+import {  Route , Routes } from "react-router-dom";
+import { Layout } from "./Layout/Layout";
+import { Home } from "./Home/Home";
+import { GiphsSearch } from "./GiphsSearch/GiphsSearch"
+import React from "react";
+import {GiphDetails} from "./GiphsDetails/GiphsDetails"
+import {GiphAuthor} from "./GiphAuthor/GiphAuthor.jsx"
 
-//  const App = () => {
-//   return (
-//     <div className="App">
+ const App = () => {
+  return (
+    <div className="App">
      
-//     <Routes>
-//       <Route path="/" element={<Layout />}>
-//           <Route index element={<Home />} />
-//           <Route path="giphs" element={<GiphsSearch />}></Route>
-//           <Route path="giphs/:giphId" element={<GiphDetails />}>
-//               <Route path="author" element={<GiphAuthor />} />
-//           </Route>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="giphs" element={<GiphsSearch />}></Route>
+          <Route path="giphs/:giphId" element={<GiphDetails />}>
+              <Route path="author" element={<GiphAuthor />} />
+          </Route>
           
-//       </Route>
-//     </Routes>
-//     </div>
-//   );
-// };
+      </Route>
+    </Routes>
+    </div>
+  );
+};
 
-// export default App;
+export default App;
 
 
 // {/* <Layout /> */}
