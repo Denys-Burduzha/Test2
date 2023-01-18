5acbebd43e6baf74e1f6df412730f5113ff46a9e
Denys-Burduzha <106137997+Denys-Burduzha@users.noreply.github.com>
2023-01-18 15:23:13 +0100

    7

--- src/components/GiphsDetails/GiphsDetails.jsx
+++ src/components/GiphsDetails/GiphsDetails.jsx
@@ -1,46 +1,46 @@
 
 
-// import { useEffect, useState } from "react";
-// import {useNavigate, useParams} from "react-router-dom";
+import { useEffect, useState } from "react";
+import {useNavigate, useParams} from "react-router-dom";
 // import { Link } from "react-router-dom";
-// import * as GiphyAPI from "../../Api/Giphy";
-// import { Outlet } from "react-router-dom";
+import * as GiphyAPI from "../../Api/Giphy";
+import { Outlet } from "react-router-dom";
 
-// export const GiphDetails = () => {
-//     const {giphId} = useParams();
-//     const navigate = useNavigate();
-//     const [giphDetails, setGiphDetails] = useState();
+export const GiphDetails = () => {
+    const {giphId} = useParams();
+    const navigate = useNavigate();
+    const [giphDetails, setGiphDetails] = useState();
 
    
 
-//     useEffect(() => {
-//         GiphyAPI.getGiphById(giphId).then(setGiphDetails)
-//     }, [giphId]);
+    useEffect(() => {
+        GiphyAPI.getGiphById(giphId).then(setGiphDetails)
+    }, [giphId]);
 
-//     console.log(giphDetails);
+    console.log(giphDetails);
 
-//     if (!giphDetails) {
-//         return null;
+    if (!giphDetails) {
+        return null;
         
-//     }
+    }
 
-//     return(
-//         <>
-//             <button onClick={() => navigate(- 1) }>Back</button>
-//             <div>
-//                 <img src={ giphDetails.images.original.url } alt={giphDetails.title} />
-//             </div>
+    return(
+        <>
+            <button onClick={() => navigate(- 1) }>Back</button>
+            <div>
+                <img src={ giphDetails.images.original.url } alt={giphDetails.title} />
+            </div>
 
-//             <div>
-//                 <h3>Giph Info</h3>
-//                 <div>{giphDetails.title}</div>
-//             </div>
-//             <hr />
-//             <div>
-//                 <h3>Actions</h3>
-//                 <button onClick={() =>  navigate("author")}>Author</button> 
-//             </div>
-//             <Outlet />
-//         </>
-//     );
-// };
+            <div>
+                <h3>Giph Info</h3>
+                <div>{giphDetails.title}</div>
+            </div>
+            <hr />
+            <div>
+                <h3>Actions</h3>
+                <button onClick={() =>  navigate("author")}>Author</button> 
+            </div>
+            <Outlet />
+        </>
+    );
+};
